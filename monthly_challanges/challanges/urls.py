9afc39801ea6cda1aac52f1dc23eed68254daf4d
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-04 21:46:50 +0530

    Adding Redirects

--- monthly_challanges/challanges/urls.py
+++ monthly_challanges/challanges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("<month>",views.monthly_challange)
+  path("<int:month>",views.monthly_challange_number),
+  path("<str:month>",views.monthly_challange)
 ]
--- monthly_challanges/challanges/views.py
+++ monthly_challanges/challanges/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse , HttpResponseNotFound
+from django.http import HttpResponse , HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challange_dict = {
+  "january":"Wake up at 6:00 am",
+  "february":"Go to Gym for 90 min",
+  "march":"Practice React Projects",
+  "april":"Complete Node Bootcamp",
+  "may":"Read for 50 min",
+  "june":"Drink 8 glasses water",
+  "july":"Consume no sugar",
+  "august":"Consume no Salt",
+   "september":"Practise node",
+  "november":"Practise aspnet",
+  "november":"Practise algorithm",
+  "december":"Practise flute",
+}
 
 # Create your views here.
+def monthly_challange_number(request,month):
+    months = list(monthly_challange_dict.keys())
+    if month > len(months):
+       return HttpResponseNotFound("Invalid month")
+    redirect_month = months[month - 1]
+
+    return HttpResponseRedirect("/challanges/" + redirect_month)
+
+
 def monthly_challange(request,month):
   challange_text = None
-  if month == "january":
-    challange_text = "Walk 20 min everyday"
-  elif month == "february":
-    challange_text = "Learn HTML CSS"
-  elif month == "march":
-    challange_text = "Learn Django"
-  else:
+  try:
+     challange_text = monthly_challange_dict[month];
+     return HttpResponse(challange_text)
+  except: 
     return HttpResponseNotFound("You have entered wrong value")
-  return HttpResponse(challange_text)
+     
