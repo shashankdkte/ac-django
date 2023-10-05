ab198d9604ae99eeafb2c9baab620ec9e415f01c
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-05 18:15:23 +0530

    Adding Static elements

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def starting_page(request):
-  return HttpResponse("StartPage")
+  return render(request, "blog/index.html")
 
 def posts(request):
   return HttpResponse("PostsPage")
