9344fe6c23c76e3b0a77c15fb6cb70cb24d8b0fa
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-08 02:56:57 +0530

    FormView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,30 +1,24 @@
 from typing import Any
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import FormView
 # Create your views here.
 
 
 
-class ReviewView(View):
-  def get(self,request):
-    form  = ReviewForm()
+class ReviewView(FormView):
+ form_class = ReviewForm
+ template_name = "reviews/review.html"
+ success_url = "/thankyou"
 
-    return render(request,"reviews/review.html",{
-      "form": form
-    })
-  
-  def post(self,request):
-    form = ReviewForm(request.POST)
-
-    if form.is_valid():
-      form.save()
-      return HttpResponseRedirect("/thankyou")
-
+ def form_valid(self, form: Any):
+   form.save()
+   return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
