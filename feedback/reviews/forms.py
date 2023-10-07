9310ffaa3981267d6bdfdfe40073138283fca152
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-08 00:30:40 +0530

    Storing in Database

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  username = forms.CharField(label="Your username", max_length=100, error_messages={
+  user_name = forms.CharField(label="Your username", max_length=100, error_messages={
     "required":"Your name cannot be empty",
     "max_length":"Please enter a shorter name"
   })
-
+  review_text = forms.CharField(label="Your Feedback",widget=forms.Textarea,max_length=200)
+  rating = forms.IntegerField(label="Your rating",min_value=1,max_value=5)
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 def review(request):
   if request.method == "POST":
     form = ReviewForm(request.POST)
 
     if form.is_valid():
-      print(form.cleaned_data)
+      Review.objects.create(
+        user_name = form.cleaned_data['user_name'],
+        review_text = form.cleaned_data['review_text'],
+        rating = form.cleaned_data['rating'],
+        )
       return HttpResponseRedirect("/thankyou")
   else:
     form  = ReviewForm()
